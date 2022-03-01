5225f41352f6991ed21fa145731dd04355543efe
Shuo Wu <wushuo2010@gmail.com>
2022-02-28 22:52:50 -0500

    send request with axios

--- scripts/dispatchEvent.js
+++ scripts/dispatchEvent.js
@@ -1,1 +1,21 @@
-console.log('test node');
+const axios = require('axios');
+
+const token = process.argv[2];
+
+axios.post({
+  url: 'https://api.github.com/repos/shuowu/gh-actions-playground-downstream/dispatches',
+  method: 'post',
+  headers: {
+    'Authorization': `token ${token}`,
+    'Accept': 'application/vnd.github.v3+json',
+    'Content-Type': 'application/json',
+  },
+  data: {
+    'event_type': 'custom_event_type',
+    'client_payload': {
+      a: 'a'
+    }
+  }
+})
+.then(res => console.log(res))
+.catch(error => console.log(error));
